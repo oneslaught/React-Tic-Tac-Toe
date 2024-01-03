05f216c3f7a001896e786653c8868b723d4d2269
oneslaught <iceminecraft021@gmail.com>
2024-01-04 00:08:20 +0200

    fixed

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,22 +1,39 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import path from 'path';
-import { Configuration } from 'webpack';
-import 'webpack-dev-server';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
+import { Configuration } from "webpack";
+import "webpack-dev-server";
 
 const webpackConfig: Configuration = {
-  entry: 'src/index.tsx',
+  mode: "production",
+  entry: "./src/index.tsx",
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+	  {
+        test: /\.css$/i,
+        use: ["css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [new HtmlWebpackPlugin()],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'),
+      directory: path.join(__dirname, "public"),
     },
     compress: true,
     port: 9000,
   },
 };
 
-export {webpackConfig};
+export default webpackConfig;
