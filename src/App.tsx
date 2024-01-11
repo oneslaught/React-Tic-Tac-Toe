39bb24b9eddcab4bb37d36e2097fc655c4ad88d1
oneslaught <iceminecraft021@gmail.com>
2024-01-11 23:41:02 +0200

    added fixed commited

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import React from "react";
-import Counter from "./components/counter";
+
 import "./styles/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Counter />
-      <Counter />
-    </div>
-  );
+export default function App() {
+  return <div className="App"></div>;
 }
-
-export default App;
---
+++ src/components/Board.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Board = () => {
+  return (
+	<div>Board</div>
+  )
+}
+
+var smt = "smth"
