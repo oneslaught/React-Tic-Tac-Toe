3e50640422f68de48882d4d05c18e02237264d3b
oneslaught <iceminecraft021@gmail.com>
2024-01-30 21:53:55 +0200

    PlayerTurn completed

--- src/components/PlayerTurn.tsx
+++ src/components/PlayerTurn.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 
 import turn from "../styles/player-turn.module.css";
+import { useGameContext } from "./GameContext";
 
 export default function PlayerTurn() {
+  const { xIsNext } = useGameContext();
+
   return (
     <div className={`${turn.container}`}>
       <h3>Turn For</h3>
       <div className={`${turn.turnBox} ${turn.align}`}>X</div>
       <div className={`${turn.turnBox} ${turn.align}`}>O</div>
-      <div className={`${turn.bg}`}></div>
+      <div className={`${turn.bg} ${!xIsNext && turn.oTurn}`}></div>
     </div>
   );
 }
